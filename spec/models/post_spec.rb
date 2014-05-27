906fc92b779baec1f3e44050db7296c91d73918d
Emma <emma_williams1@hotmail.co.uk>
2014-05-27 15:39:00 +0100

    index is functional

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+# describe  do
+#     it 'is not valid without a name' do 
+#       post = Post.new(title: nil)
+#       expect(post).to have(1).errors_on(:title)
+#     end
+    # it 'should have a title' do
+    #   visit '/instamaker_pages/home'
+    #   page.should have_title('h1', :text => 'Instamaker')
+         # end
