e901a5a1dda42c7b3ac8674a04d7deb5591e8622
Emma <emma_williams1@hotmail.co.uk>
2014-06-02 18:28:39 +0100

    set up many to many relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ActiveRecord::Base
   }
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
+  has_and_belongs_to_many :tags
 
   def tag_names
     ''
